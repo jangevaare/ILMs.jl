813fb3a7ed3c0f922f9f712885b0da50e5b915be
jangevaa <jangevaa@uoguelph.ca>
2014-10-16 17:20:55 -0400

    Finish distance matrix function

--- population.jl
+++ population.jl
@@ -14,20 +14,12 @@ function distance_mat_fun(pop_db)
   Construct a matrix containing the euclidean distance between every
   pair of individuals
   """
-  distance_mat = Array(dims=(pop_db[end, :ind_id],pop_db[end, :ind_id]))
-  for i = pop_db[:ind_id]
-    for j = pop_db[:ind_id]
-      euclidean([pop_db[i, 2],pop_db[i, 3]], [pop_db[j, 2],pop_db[j, 3]])
-
-
-pop_db1 = pop_db_fun(100)
-sample(1:100)
-
-      euclidean([pop_db1[1, 2],pop_db1[1, 3]], [pop_db1[2, 2],pop_db1[2, 3]])
-
-
-      pop_db1[end,:ind_id]
-
-DataFrame(ind_id = sample(1:100), newstatus = ['i'])
-
-      Array(Int64, 2)
+  distance_mat = zeros(pop_db[end, :ind_id],pop_db[end, :ind_id])
+  for i = 1:pop_db[end,:ind_id]
+    for j = (i+1):pop_db[end, :ind_id]
+      distance_mat[j,i] = euclidean([pop_db[i, 2],pop_db[i, 3]], [pop_db[j, 2],pop_db[j, 3]])
+      distance_mat[i,j] = distance_mat[j,i]
+    end
+  end
+  distance_mat
+end
