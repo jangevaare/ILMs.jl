77d633452a74a05ae024af1509d0f5f39dcd2313
jangevaa <jangevaa@uoguelph.ca>
2014-10-18 12:54:21 -0400

    create functions for arrays of susceptible and infectious individuals - only useful for discrete time models

--- inference.jl
+++ inference.jl
@@ -1,8 +1,35 @@
 include("events.jl")
 using Distributions
 
-function SIR_loglikelihood(distance_mat, recovery_times, event_db, alpha, beta, gamma_inverse)
+function infectious_array_fun(event_db, obs_length)
+"""
+Save the liklihood function from repetively determining infectious status,
+by producing an array which contains this information for all relevant
+time steps
+"""
+  infectious_array=falses(size(event_db)[1], obs_length)
+  for i = 1:obs_length
+    infectious_array[:,i] = find_infectious_fun(event_db, i)
+  end
+  infectious_array
+end
+
+function susceptible_array_fun(event_db, obs_length)
+"""
+Save the liklihood function from repetively determining susceptible status,
+by producing an array which contains this information for all relevant
+time steps
+"""
+  susceptible_array=falses(size(event_db)[1], obs_length)
+  for i = 1:obs_length
+    susceptible_array[:,i] = find_susceptible_fun(event_db, i)
+  end
+  susceptible_array
+end
+
+function SIR_loglikelihood(distance_mat, recovery_times, event_db, alpha, beta, gamma_inverse, obs_length)
   loglikelihood(Geometric(gamma_inverse), recovery_times) +
-  daily_loglikes = zeros(convert(int64, event_db[end, :time])-1)
+  daily_loglikes = zeros(obs_length-1)
   for i = 1:length(daily_loglikes)
+    infectious = find_infectious_fun
     daily_loglikes[i] = infect_prob_fun(distance_mat, infectious, susceptible, alpha, beta)
