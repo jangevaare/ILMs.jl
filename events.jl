1dde98b851c142b6a64889864a1646ff3ec64ddd
jangevaa <jangevaa@uoguelph.ca>
2014-10-18 13:54:04 -0400

    allow for recovery times to have na's removed, or included if desired

--- events.jl
+++ events.jl
@@ -21,7 +21,7 @@ Find individuals which have not been infected prior to `time`
 """
   susceptible_index = falses(size(event_db)[1])
   for i = 1:length(susceptible_index)
-    susceptible_index[i] = isnan(event_db[i,3]) || event_db[i,3] > time
+    susceptible_index[i] = isnan(event_db[i,3]) || event_db[i,3] >= time
   end
   susceptible_index
 end
@@ -32,7 +32,7 @@ Find individuals which have been infected prior to `time`, but have not recovere
 """
   infectious_index = falses(size(event_db)[1])
   for i = 1:length(infectious_index)
-    infectious_index[i] = event_db[i,3] <= time && ((isnan(event_db[i,4])) || (time < event_db[i,4]))
+    infectious_index[i] = event_db[i,3] < time && ((isnan(event_db[i,4])) || (time <= event_db[i,4]))
   end
   infectious_index
 end
@@ -48,11 +48,18 @@ Find individuals which have recovered prior to `time`
   recovered_index
 end
 
-function find_recovery_times(event_db)
+function find_recovery_times(event_db, narm)
 """
 Determine recovery times for individuals
 """
-event_db[:,4]  - event_db[:,3]
+  if narm == false
+    event_db[:,4]  - event_db[:,3]
+  elseif narm == true
+    times = event_db[:,4]  - event_db[:,3]
+    times[isnan(times).==false]
+  else
+    error("narm (2nd argument) must be boolean")
+  end
 end
 
 function infect_prob_fun(distance_mat, infectious, susceptible, alpha, beta)
@@ -67,8 +74,9 @@ function infect_fun(distance_mat, event_db, time, alpha, beta)
 Propagate infection through population according to `alpha` and `beta`
 """
   susceptible = find_susceptible_fun(event_db, time)
-  infect_probs=infect_prob_fun(distance_mat, find_infectious_fun(event_db, time), susceptible, alpha, beta)
-  infected = falses(length(infect_probs))
+  infect_probs = zeros(length(susceptible))
+  infect_probs[susceptible]=infect_prob_fun(distance_mat, find_infectious_fun(event_db, time), susceptible, alpha, beta)
+  infected = falses(length(susceptible))
   for i in 1:length(infect_probs)
     infected[i] = rand() < infect_probs[i]
   end
@@ -82,7 +90,7 @@ Recover individuals following a geometric distribution with p = `gamma_inverse`
   infectious = find_infectious_fun(event_db, time)
   recovered = falses(length(infectious))
   for i = 1:length(infectious)
-    recovered[i] = infectious[i] && rand() < gamma_inverse
+    recovered[i] = infectious[i] && (rand() < gamma_inverse)
   end
-  event_db[recovered, 4] = time
+  event_db[recovered, 4] = rep(time, sum(recovered))
 end
