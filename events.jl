d5c1127cad43da4108ff49b9e7b342a650f1de56
jangevaa <jangevaa@uoguelph.ca>
2014-10-18 12:07:04 -0400

    Complete conversion to new DB format

--- events.jl
+++ events.jl
@@ -37,54 +37,22 @@ Find individuals which have been infected prior to `time`, but have not recovere
   infectious_index
 end
 
-
-
-
-function find_infectious_fun(event_db, time, when)
+function find_recovered_fun(event_db, time)
 """
-Find individuals which have been infected, but have not recovered prior to, or at `time`
+Find individuals which have recovered prior to `time`
 """
-  if when =='now'
-    event_db[(event_db[:time] .== time) & (event_db[:newstatus] .== 'i'),1]
+  recovered_index = falses(size(event_db)[1])
+  for i = 1:length(recovered_index)
+    recovered_index[i] = event_db[i,4] < time
   end
-  if when =='prior'
-    infected = event_db[(event_db[:time] .< time) & (event_db[:newstatus] .== 'i'),1]
-    recovered = event_db[(event_db[:time] .< time) & (event_db[:newstatus] .== 'r'),1]
-    if length(recovered) > 0
-      step1 = trues(length(infected))
-      for i = 1:length(infected)
-        step1[i] = any(recovered .== infected[i]) == false
-      end
-      infected[step1]
-    else
-      infected
-    end
-  end
-end
-
-function find_recovered_fun(event_db, time, when)
-"""
-Find individuals which have recovered at, or prior to `time`
-"""
-  if when == 'now'
-    event_db[(event_db[:time] .== time) & (event_db[:newstatus] .== 'r'),1]
-  end
-  if when == 'prior'
-    event_db[(event_db[:time] .< time) & (event_db[:newstatus] .== 'r'),1]
-  end
+  recovered_index
 end
 
 function find_recovery_times(event_db)
 """
 Determine recovery times for individuals
 """
-  recovered_db=event_db[event_db[:newstatus] .== 'r', :]
-  infected_db=event_db[event_db[:newstatus] .== 'i', :]
-  recovery_times = zeros(size(recovered_db)[1])
-  for i = 1:length(recovery_times)
-    recovery_times[i] = recovered_db[i,:time] - infected_db[infected_db[:ind_id] .== recovered_db[i,:ind_id],:time][1]
-  end
-  recovery_times
+event_db[:,4]  - event_db[:,3]
 end
 
 function infect_prob_fun(distance_mat, infectious, susceptible, alpha, beta)
@@ -104,7 +72,7 @@ Propagate infection through population according to `alpha` and `beta`
   for i in 1:length(susceptible)
     infected[i] = rand() < infect_probs[i,1]
   end
-  append!(event_db, DataFrame(ind_id = susceptible[infected], newstatus = rep('i', sum(infected)), time = rep(time, sum(infected))))
+  event_db[infected, 3] = time
 end
 
 function recover_fun(event_db, time, gamma_inverse)
@@ -114,8 +82,7 @@ Recover individuals following a geometric distribution with p = `gamma_inverse`
   infectious = find_infectious_fun(event_db, time)
   recovered = falses(length(infectious))
   for i = 1:length(infectious)
-    recovered[i] = rand() < gamma_inverse
+    recovered[i] = infectious[i] && rand() < gamma_inverse
   end
-  append!(event_db, DataFrame(ind_id = infectious[recovered], newstatus = rep('r', sum(recovered)), time = rep(time, sum(recovered))))
+  event_db[recovered, 4] = time
 end
-
