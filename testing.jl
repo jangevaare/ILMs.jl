ac728689c7275716c1fcddfd65b2e4285f012479
jangevaa <jangevaa@uoguelph.ca>
2014-10-17 22:26:53 -0400

    Infection function

--- testing.jl
+++ testing.jl
@@ -11,3 +11,10 @@ test1 = find_infectious_fun(edb, 2)
 test2 = find_susceptible_fun(edb, 2)
 test3 = find_recovered_fun(edb, 2)
 
+infectious1 = find_infectious_fun(edb, 2)
+susceptible1 = find_susceptible_fun(edb, 2)
+infect_prob1=infect_prob_fun(dist_mat1, infectious1, susceptible1, 1, 8)
+
+infect_fun(dist_mat1, edb, 2., 1, 5)
+edb
+
