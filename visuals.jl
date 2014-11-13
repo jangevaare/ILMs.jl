4ff8755bb780c6a321e217d4cacacd760ca06313
jangevaa <jangevaa@uoguelph.ca>
2014-11-13 10:07:26 -0500

    Fix event timeseries function

--- visuals.jl
+++ visuals.jl
@@ -2,23 +2,22 @@ function state_timeseries(event_db, cd="discrete")
 	"""
 	Count the number of individuals in each state at each event time
 	"""
-	susceptible = fill(0, length(event_db.event_times))
-	infectious = fill(0, length(event_db.event_times))
+	susceptible = fill(0, length(event_db.event_times[event_db.event_times .< Inf]))
+	infectious = fill(0, length(event_db.event_times[event_db.event_times .< Inf]))
 	if(size(event_db.events)[2] == 4)
-		recovered = fill(0, length(event_db.event_times))
-		for t = 1:length(event_db.event_times)
+		recovered = fill(0, length(event_db.event_times[event_db.event_times .< Inf]))
+		for t = 1:length(event_db.event_times[event_db.event_times .< Inf])
 			susceptible[t] = sum(find_state(event_db, event_db.event_times[t], "S", cd))
 			infectious[t] = sum(find_state(event_db, event_db.event_times[t], "I", cd))
 			recovered[t] = sum(find_state(event_db, event_db.event_times[t], "R", cd))
 		end
-		return DataFrame(time = event_db.event_times, S = susceptible, I = infectious, R = recovered)
+		return DataFrame(time = event_db.event_times[event_db.event_times .< Inf], S = susceptible, I = infectious, R = recovered)
 	end
     if(size(event_db.events)[2] == 3)
-        recovered = fill(0, length(event_db.event_times))
-        for t = 1:length(event_db.event_times)
+        for t = 1:length(event_db.event_times[event_db.event_times .< Inf])
             susceptible[t] = sum(find_state(event_db, event_db.event_times[t], "S", cd))
             infectious[t] = sum(find_state(event_db, event_db.event_times[t], "I", cd))
         end
-        return DataFrame(time = event_db.event_times, S = susceptible, I = infectious)
+        return DataFrame(time = event_db.event_times[event_db.event_times .< Inf], S = susceptible, I = infectious)
     end
 end
