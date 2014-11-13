e93ff58cd672c2bde06a64f9d4ddbc9e6f4be746
jangevaa <jangevaa@uoguelph.ca>
2014-11-13 10:24:25 -0500

    Adjustments to timeseries function for new edb type

--- visuals.jl
+++ visuals.jl
@@ -7,16 +7,16 @@ function state_timeseries(event_db)
 	if(size(event_db.events)[2] == 4)
 		recovered = fill(0, length(event_db.event_times[event_db.event_times .< Inf]))
 		for t = 1:length(event_db.event_times[event_db.event_times .< Inf])
-			susceptible[t] = sum(find_state(event_db, event_db.event_times[t], "S", event_db.cd))
-			infectious[t] = sum(find_state(event_db, event_db.event_times[t], "I", event_db.cd))
-			recovered[t] = sum(find_state(event_db, event_db.event_times[t], "R", event_db.cd))
+			susceptible[t] = sum(find_state(event_db, event_db.event_times[t], "S"))
+			infectious[t] = sum(find_state(event_db, event_db.event_times[t], "I"))
+			recovered[t] = sum(find_state(event_db, event_db.event_times[t], "R"))
 		end
 		return DataFrame(time = event_db.event_times[event_db.event_times .< Inf], S = susceptible, I = infectious, R = recovered)
 	end
     if(size(event_db.events)[2] == 3)
         for t = 1:length(event_db.event_times[event_db.event_times .< Inf])
-            susceptible[t] = sum(find_state(event_db, event_db.event_times[t], "S", event_db.cd))
-            infectious[t] = sum(find_state(event_db, event_db.event_times[t], "I", event_db.cd))
+            susceptible[t] = sum(find_state(event_db, event_db.event_times[t], "S"))
+            infectious[t] = sum(find_state(event_db, event_db.event_times[t], "I"))
         end
         return DataFrame(time = event_db.event_times[event_db.event_times .< Inf], S = susceptible, I = infectious)
     end
