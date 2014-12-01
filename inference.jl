6b3cf8fa0ce2f84ec01d6c15240ebbe6f4c0263c
jangevaa <jangevaa@uoguelph.ca>
2014-12-01 11:01:47 -0500

    update si inference example

--- inference.jl
+++ inference.jl
@@ -133,30 +133,3 @@ function create_loglikelihood(pop_db, event_db::edb)
     return dSI_ll
   end
 end
-
-#=
-function create_sir_loglikelihood(distance_mat, susceptible_array, infectious_array, recovery_times)
-"""
-Create the SIR loglikelihood function, which accepts only 3 parameters,
-alpha, beta, and gamma_inverse
-"""
-  function sir_loglikelihood(parameters)
-  """
-  Compute the log likelihood of discrete time SIR models (alpha, beta, gamma)
-  """
-    distance_mat_alphabeta = distance_mat_alphabeta_fun(distance_mat, parameters[1], parameters[2])
-    if parameters[1] <= 0 || parameters[2] <= 0 || parameters[3] >= 1 || parameters[3] <= 0
-      -Inf
-    else
-      daily_loglikes = zeros(sum(sum(susceptible_array,1) .> 0)-1)
-      for i = 1:length(daily_loglikes)
-        infect_probs=nans(size(susceptible_array)[1])
-        infect_probs[susceptible_array[:,i]]=infect_prob_fun(distance_mat_alphabeta, infectious_array[:,i], susceptible_array[:,i])
-        daily_loglikes[i]=sum(log([(infect_probs[susceptible_array[:,i] & infectious_array[:,i+1]]), 1.-(infect_probs[susceptible_array[:,i] & susceptible_array[:,i+1]])]))
-      end
-      loglikelihood(Geometric(parameters[3]), recovery_times) + logpdf(Uniform(0,1), parameters[3]) + logpdf(Gamma(1,1), parameters[1]) + logpdf(Gamma(5,1), parameters[2]) + sum(daily_loglikes)
-    end
-  end
-sir_loglikelihood
-end
-=#
