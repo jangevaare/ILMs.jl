f4bb8fc5cb7f4249a787eb25f232c81e3963637d
jangevaa <jangevaa@uoguelph.ca>
2014-12-10 22:40:37 -0500

    restructure package

--- src/ilmtools.jl
+++ src/ilmtools.jl
@@ -1,6 +1,9 @@
+module ilmtools
+
 using DataFrames, Distances, Distributions, Gadfly, Interact, Mamba
 include("population.jl")
 include("events.jl")
 include("visuals.jl")
 include("inference.jl")
 
+end # module
