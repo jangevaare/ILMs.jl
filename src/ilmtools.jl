6bfcdd36a4d917bbc5ea576d9b5a9ad63e7423d3
Justin Angevaare <jangevaa@uoguelph.ca>
2015-05-25 14:13:33 -0400

    update ilmtools.jl

--- src/ilmtools.jl
+++ src/ilmtools.jl
@@ -1,6 +1,16 @@
 module ilmtools
 
 using DataFrames, Distances, Distributions, Gadfly, Interact, Mamba
+
+export
+  create_event_db,
+  create_pop_db_univariates,
+  create_pop_db_bivariate,
+  infect_recover_loop,
+  state_timeseries,
+  state_animation,
+  create_loglikelihood
+
 include("population.jl")
 include("events.jl")
 include("visuals.jl")
